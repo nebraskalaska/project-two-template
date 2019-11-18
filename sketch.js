678b73b713557967c99f723a0c7634ca1d4ead1f
Kris <kbla2@pdx.edu>
2019-11-18 15:58:02 -0800

    Update 11/18/2019
    Splash page: Created P5.JS files

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 const barWidth = 20;
 let lastBar = -1;
 
+function preload() {
+  img = loadImage('/images/logo_white.png');
+}
+
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(windowWidth, windowHeight);
   colorMode(HSB, height, height, height);
   noStroke();
   background(height);
+  imageMode(CENTER)
 }
 
 function draw() {
@@ -16,4 +21,9 @@ function draw() {
     rect(barX, 0, barWidth, height);
     lastBar = whichBar;
   }
+  image(img, width/2, height/2, img.width/2, img.height/2);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
