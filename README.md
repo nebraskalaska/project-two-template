f46337e5b1fb52b8341f39648f98825a5ed1c08f
Kris <kbla2@pdx.edu>
2019-12-03 19:44:56 -0800

    12.3.19 7:43pm
    Final tweaks:
    Updated metadata, prettied up code, added alt-text, added fav icon, added "enter" instructions to index.html, created readme file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# project-two-template
-Project two template for des 341
+Kris Blackmore
+DES341, Fall 2019
+
+This spec site was created to show the ideal layout and functionality of a potential redesign for the Disjecta Community Arts Center webiste. As a result only the top two links on the menu are clickable, as well as the first entry on the calendar page. 
+
+User Path For Spec Site:
+Splash Page (index.html) > Home Page > Location + Contact > Calendar Page > Portland2019 Biennial (First Calendar Entry)
+
+Image Credits:
+All images were collected from the Disjecta website to make this spec site.
+www.disjecta.com
--- sketch.js
+++ sketch.js
@@ -3,17 +3,19 @@ let lastBar = -1;
 
 function preload() {
   img = loadImage('images/logo_white.png');
+  montserrat = loadFont('fonts/Montserrat-Regular.ttf')
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, height, height, height);
   noStroke();
   background(height);
   imageMode(CENTER)
 }
 
 function draw() {
+  colorMode(HSB, height, height, height);
+
   let whichBar = mouseX / barWidth;
   if (whichBar !== lastBar) {
     let barX = whichBar * barWidth;
@@ -22,6 +24,13 @@ function draw() {
     lastBar = whichBar;
   }
   image(img, width/2, height/2, img.width/2, img.height/2);
+
+  colorMode(RGB);
+  fill(255);
+  textSize(22);
+  textAlign(CENTER);
+  textFont(montserrat);
+  text('click to enter', width/2, (height/2)+(img.height/3));
 }
 
 function windowResized() {
